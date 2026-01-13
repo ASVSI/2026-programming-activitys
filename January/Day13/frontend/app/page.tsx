e142adbe067cefe671503a51c474b9dd82491627
ASVSI <asvsi.com@gmail.com>
2026-01-13 11:55:03 +0000

    added frontend

---
+++ January/Day13/frontend/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <form>
+          <input type="text" placeholder="Enter Number" />
+          <button>SetNumber</button>
+        </form>
+        <button>view number</button>
+      
+    </div>
+  );
+}
